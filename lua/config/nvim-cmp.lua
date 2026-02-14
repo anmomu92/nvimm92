ea22d020ac696299566608ac11a53216932d08a0
Antonio Morán Muñoz <anmomu92@gmail.com>
2026-02-14 11:59:51 +0100

    Disabled autocompletion selected by default

--- lua/config/nvim-cmp.lua
+++ lua/config/nvim-cmp.lua
@@ -2,6 +2,14 @@ local cmp = require('cmp')
 require("luasnip.loaders.from_vscode").lazy_load()
 
 cmp.setup({
+	enabled = function()
+		local disabled = false
+		disabled = disabled or (vim.api.nvim_get_option_value('buftype', { buf = 0 }) == 'prompt')
+		disabled = disabled or (vim.fn.reg_recording() ~= '')
+		disabled = disabled or (vim.fn.reg_executing() ~= '')
+		disabled = disabled or require('cmp.config.context').in_treesitter_capture('comment')
+		return not disabled
+	end,
     snippet = {
       -- REQUIRED - you must specify a snippet engine
       expand = function(args)
@@ -19,7 +27,7 @@ cmp.setup({
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
       ['<C-Space>'] = cmp.mapping.complete(),
       ['<C-e>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+	  ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     }),
     sources = cmp.config.sources({
       -- { name = 'nvim_lsp' },
