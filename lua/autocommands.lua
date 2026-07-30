85c85df82ff75fc1573ab6ad8ca280e41c3ad036
Antonio Morán Muñoz <anmomu92@gmail.com>
2026-07-30 12:22:00 +0200

    added: autocommand to automatically center the cursor

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -5,25 +5,39 @@ local cmd = vim.cmd
 
 -- Functions to run when writing a Buffer
 api.nvim_create_autocmd({ "BufWritePost" }, {
-  callback = function()
-    require("lint").try_lint()
-	require("taskman").UpdateTasks()
-  end,
+	callback = function()
+		require("lint").try_lint()
+		require("taskman").UpdateTasks()
+	end,
 })
 
 -- Funtions to run when entering Vim
 api.nvim_create_autocmd("VimEnter", {
-  callback = function()
-    require("lazy").update({ show = false })
-  end
+	callback = function()
+		require("lazy").update({ show = false })
+	end
 })
 
 -- User commands
 -- Vertical help
 api.nvim_create_user_command("Help", function(opts)
-  cmd("vertical help " .. opts.args)
+	cmd("vertical help " .. opts.args)
 end, { nargs = "?", complete = "help" })
 
 -- Alias :h to :Help
 cmd("cnoreabbrev <expr> h getcmdtype() == ':' && getcmdline() == 'h' ? 'Help' : 'h'")
 
+-- autocenter the cursor
+local group = vim.api.nvim_create_augroup("AutoCenter", { clear = true })
+
+local last_line = 0
+vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+	group = group,
+	callback = function()
+		local line = vim.fn.line(".")
+		if line ~= last_line then
+			last_line = line
+			vim.cmd("normal! zz")
+		end
+	end,
+})
